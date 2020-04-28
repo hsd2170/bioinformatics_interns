c26b5eb23516ffd297bfbc7b044820894823684e
hsd2170 <53091477+hsd2170@users.noreply.github.com>
2020-04-28 17:08:17 -0400

    Update kmer.py

--- kmer.py
+++ kmer.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
+#import modules
 from collections import Counter
 from collections import OrderedDict
 
+#create function that takes two parameters
+#create a list that searches for sets of similar nucleotides by "stepping" through a sequence and recording each of parameter length with corresponding base
 def get_all_k_mer(string, k=1):
+#find the length of the sequence
    length = len(string)
-   return [string[i: i+ k] for i in range(length-k+1)]
+#obtain list output from function   
+	return [string[i: i+ k] for i in range(length-k+1)]
 
+#open a file to read, replace linefeed with space
 with open ("/Users/nhenhunter/rosalind/og_seq.fasta", "r") as in_handle:
 	string = in_handle.read().replace('\n', '')
 
 print(string)
+#call function and provide string and kmer length as parameters
+#create a dictionary using a module
 kmer_dict = Counter(get_all_k_mer(string, k=6))
 
-
+#print dictionary
 print(kmer_dict)
 
 
 
 
 #obtain list output from function
-#import modules 
-#find the length of the sequence
-#open a file to read, replace linefeed with space
-#create a dictionary using a module
-#call function and provide string and kmer length as parameters
-#print dictionary
-#create a list that searches for sets of similar nucleotides by "stepping" through a sequence and recording each of parameter length with corresponding base pairs
-#create function that takes two parameters
+ 
+
+
+
+
+
